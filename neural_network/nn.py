f34b3ce0a8bd80c1d0d0e5677a98d31a00622bd0
Raghu <raghu15sep99@gmail.com>
2020-04-26 17:52:47 +0530

    added loss curve

--- neural_network/nn.py
+++ neural_network/nn.py
@@ -3,6 +3,7 @@ import pandas as pd
 from utils import train_test_split
 from layer import Layer
 from dropout import Dropout
+import matplotlib.pyplot as plt
 
 np.random.seed(1234)
 eps = np.finfo(np.float32).eps
@@ -18,6 +19,8 @@ class NeuralNetwork:
         '''
         self.load_data(filename)
         self.layers = []
+        self.history_loss = []
+        self.history_val_loss = []
     
     def load_data(self, filename):
         df = pd.read_csv(filename, header=None)
@@ -28,6 +31,7 @@ class NeuralNetwork:
         self.d = X.shape[1]
         self.out = self.d
         self.X_train, self.y_train, self.X_test, self.y_test = train_test_split(X, y)
+        self.y_test = self.y_test.reshape(-1, 1)
     
     def add_layer(self, nodes, activation='relu'):
         '''
@@ -50,7 +54,11 @@ class NeuralNetwork:
     def launch_training(self, epochs=1000, learning_rate=0.05, batch_size=64):
         self.train(self.X_train, self.y_train, epochs, learning_rate, batch_size)
         print()
+        print('Test')
         accuracy, f1score = self.evaluate()
+        print('Accuracy: {:.3f}\nF1-score {:.3f}'.format(accuracy, f1score))
+        print('Train')
+        accuracy, f1score = self.evaluate(use_training_data=True)
         print('Accuracy: {:.3f}\nF1-score {:.3f}'.format(accuracy, f1score))
 
     def evaluate(self, use_training_data=False):
@@ -91,8 +99,15 @@ class NeuralNetwork:
             # Compute loss
             yhat = self.predict(X_train, training=False)
             loss = self.cross_entropy_loss(yhat, y_train)
+            self.history_loss.append(loss)
             print(f'Epoch: {epoch + 1}, loss: {loss}', end='\r')
 
+            # validation loss
+            yhat = self.predict(self.X_test, training=False)
+            loss = self.cross_entropy_loss(yhat, self.y_test)
+            self.history_val_loss.append(loss)
+
+            # Evaluate model at step
             if (epoch + 1) % 500 == 0:
                 print()
                 accuracy, f1score = self.evaluate()
@@ -150,38 +165,49 @@ def logistic_regression():
     nn = NeuralNetwork()
     nn.add_layer(1, activation='sigmoid')
     nn.launch_training(epochs=10000, learning_rate=0.1, batch_size=32)
+    return nn
 
 def neural_net():
     '''
     Neural network architecture
     '''
-    # Accuracy: 0.925 F1-score 0.923
+    # Accuracy: 0.928 F1-score 0.926
     nn = NeuralNetwork()
     nn.add_layer(8)
     nn.add_layer(8)
     nn.add_layer(8)
     nn.add_layer(8)
     nn.add_layer(1, activation='sigmoid')
-    nn.launch_training(epochs=1000, learning_rate=0.05, batch_size=64)
+    nn.launch_training(epochs=1100, learning_rate=0.05, batch_size=64)
+    return nn
 
 def neural_net_with_dropout():
     '''
     Neural network architecture
     '''
-    # Accuracy: 0.925 F1-score 0.925
+    # Accuracy: 0.925 F1-score 0.924
     nn = NeuralNetwork()
-    nn.add_layer(16)
+    nn.add_layer(64)
     nn.add_dropout(0.8)
-    nn.add_layer(16)
+    nn.add_layer(32)
     nn.add_dropout(0.8)
     nn.add_layer(8)
-    nn.add_dropout(0.8)
     nn.add_layer(1, activation='sigmoid')
-    nn.launch_training(epochs=2000, learning_rate=0.2, batch_size=64)
-    nn.launch_training(epochs=500, learning_rate=0.01, batch_size=64)
+    nn.launch_training(epochs=1000, learning_rate=0.1, batch_size=64)
+    return nn
 
 if __name__ == "__main__":
-    # logistic_regression()
-    neural_net()
-    # neural_net_with_dropout()
+    # nn = logistic_regression()
+    nn = neural_net()
+    # nn = neural_net_with_dropout()
+
+    # plot loss curve
+    x = np.arange(len(nn.history_loss))
+    plt.plot(x, nn.history_loss, label='Train loss')
+    plt.plot(x, nn.history_val_loss, label='Validation loss')
+    plt.xlabel('No of iterations')
+    plt.ylabel('Average cross entropy loss')
+    plt.title('Loss curve')
+    plt.legend()
+    plt.savefig('plots/train_loss.eps', format='eps')
 
